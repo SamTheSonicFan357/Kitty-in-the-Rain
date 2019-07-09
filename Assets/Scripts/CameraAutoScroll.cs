56b8db357f69e830a2b4355413b691c0e72d8180
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-07-09 07:40:12 -0500

    Added Auto Scroll feature to camera.

---
+++ Assets/Scripts/CameraAutoScroll.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAutoScroll : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(scrollSpeed, 0f, 0f);
+    }
+}
