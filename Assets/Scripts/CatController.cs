e924b1fc5d322ac2d8718583c947a93b754d9293
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-07-17 21:48:05 -0500

    Added Frictionless material to box collider of cat, Made the cat jumpable, and the stage spawns infinte ground tiles.

---
+++ Assets/Scripts/CatController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public int jumpForce;
+
+    private Rigidbody2D rigBody;
+    private bool hasJumped = false;
+    private bool isGrounded = false;
+
+    private const float groundedRadius = .2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            hasJumped = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = CheckIfGrounded();
+
+        if (hasJumped)
+        {
+            rigBody.AddForce(new Vector2(0f, jumpForce));
+            hasJumped = false;
+        }
+    }
+
+    private bool CheckIfGrounded()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GroundGenerator : MonoBehaviour
+{
+    public Tilemap tileMap;
+    public Tile groundTile;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private const int groundWidth = 1;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3Int currentCell = tileMap.WorldToCell(transform.position);
+
+        if (transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + distanceBetween + groundWidth, transform.position.y);
+            tileMap.SetTile(currentCell, groundTile);
+            //Instantiate(groundTile, transform);
+        }
+    }
+}
