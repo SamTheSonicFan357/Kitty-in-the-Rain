4001647110500315f584eb2c7014f1be7daf1003
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-07-20 14:01:25 -0500

    Spawn initial ground tiles to fit the screen.

--- Assets/Scripts/GroundGenerator.cs
+++ Assets/Scripts/GroundGenerator.cs
@@ -12,15 +12,34 @@ public class GroundGenerator : MonoBehaviour
 
     private const int groundWidth = 1;
 
+    private void Start()
+    {
+        int screenHeight = (int)(2 * Camera.main.orthographicSize);
+        int screenWidth = (int)(screenHeight * Camera.main.aspect + 1);
+
+        for (int i = 0; i < screenWidth; i++)
+        {
+            Vector3Int currentCell = tileMap.WorldToCell(transform.position);
+
+            if (transform.position.x < generationPoint.position.x)
+            {
+                Vector3 translation = new Vector3(distanceBetween + groundWidth, 0f);
+                transform.position += translation;
+                generationPoint.position += translation;
+                tileMap.SetTile(currentCell, groundTile);
+            }
+        }
+    }
+
     private void Update()
     {
         Vector3Int currentCell = tileMap.WorldToCell(transform.position);
 
         if (transform.position.x < generationPoint.position.x)
         {
-            transform.position = new Vector3(transform.position.x + distanceBetween + groundWidth, transform.position.y);
+            Vector3 translation = new Vector3(distanceBetween + groundWidth, 0f);
+            transform.position += translation;
             tileMap.SetTile(currentCell, groundTile);
-            //Instantiate(groundTile, transform);
         }
     }
 }
