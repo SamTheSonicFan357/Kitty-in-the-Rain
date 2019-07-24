f33c11b6743b4378359856e6a6673f8adb441427
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-07-23 21:28:09 -0500

    (BUG_FIX) Camera follows cat even when cat is colliding

--- Assets/Scripts/CameraAutoScroll.cs
+++ Assets/Scripts/CameraAutoScroll.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class CameraAutoScroll : MonoBehaviour
 {
     public float scrollSpeed;
+    public GameObject player;
 
-    void FixedUpdate()
+    private Vector3 offset;
+
+    private void Start()
     {
-        transform.position += new Vector3(scrollSpeed, 0f, 0f);
+        offset = player.transform.position - transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        player.transform.position += new Vector3(scrollSpeed, 0f);
+        transform.position = new Vector3(player.transform.position.x - offset.x, transform.position.y, transform.position.z);
     }
 }
