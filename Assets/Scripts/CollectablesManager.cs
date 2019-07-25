90803d8d46d8201fdf9d162cddc0994afad7a958
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-07-24 21:44:30 -0500

    Removed background from fish sprite image.  Added Prefab Brush to efficiently add fish sprites on specific tiles. Painted a strait line of fish collectables through script.

---
+++ Assets/Scripts/CollectablesManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+public class CollectablesManager : MonoBehaviour
+{
+    public Tilemap tileMap;
+    public GridBrush brush;
+    public GameObject collectable;
+    public Transform generationPoint;
+    //public Transform deletionPoint;
+    public float distanceBetween;
+
+    private const int platformWidth = 1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 translation = new Vector3(distanceBetween + platformWidth, 0f);
+        transform.position += translation;
+
+        Vector3Int currentCell = tileMap.WorldToCell(transform.position + new Vector3(0f, 0.5f));
+        brush.Paint(tileMap, collectable, currentCell);
+    }
+}
