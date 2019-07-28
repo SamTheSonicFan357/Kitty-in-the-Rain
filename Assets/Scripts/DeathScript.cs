79ff65cdf7d998c2c2e0d4eb2eaa480c380ccd95
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-07-28 10:05:44 -0500

    Restart stage when cat falls

---
+++ Assets/Scripts/DeathScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScript : MonoBehaviour
+{
+    private Scene scene;
+
+    private void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Here");
+        if (other.gameObject.tag == "BottomOfScreen")
+        {
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
